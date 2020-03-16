f51fbc492872027a03dd1daed037af8bd62ed819
YaoPeiWei <2275293160@qq.com>
2020-03-17 00:27:27 +0800

    feat(map): 新增地图标注位置的功能

--- src/modular/Map/components/Map.vue
+++ src/modular/Map/components/Map.vue
@@ -1,27 +1,29 @@
 <template>
     <div class="amap-page-container">
         <div :style="{width:'100%',height:'300px'}">
-            <el-amap vid="amap" :plugin="plugin" class="amap-demo" :center="center">
+            <el-amap vid="amap" :plugin="plugin" class="amap-demo" :center="center" :markers="markers">
+                <el-amap-marker v-for="u in markers" :position="u.position" :key="u.id" :label="u.label"></el-amap-marker>
             </el-amap>
         </div>
 
-        <div class="toolbar">
-        <span v-if="loaded">
-          location: lng = {{ lng }} lat = {{ lat }}
-        </span>
-            <span v-else>正在定位</span>
-        </div>
-        <div
-                v-on:click="req_post()"
-        >
-            查询周边
-        </div>
+<!--        <div class="toolbar">-->
+<!--        <span v-if="loaded">-->
+<!--          location: lng = {{ lng }} lat = {{ lat }}-->
+<!--        </span>-->
+<!--            <span v-else>正在定位</span>-->
+<!--        </div>-->
+<!--        <div-->
+<!--                v-on:click="req_post()"-->
+<!--        >-->
+<!--            查询周边-->
+<!--        </div>-->
     </div>
 </template>
 
 <script>
 export default {
   name: 'Map',
+  props: ['markers'],
   data () {
     const self = this
     return {
--- src/modular/Map/components/MapIndex.vue
+++ src/modular/Map/components/MapIndex.vue
@@ -9,7 +9,7 @@
             <span slot="communityLocation" slot-scope="item"> <a-icon type="car" />{{item.name}} </span>
             <a href="javascript:;" slot="extra" @click="moveUserPage">GO TO USER PAGE  <a-icon type="double-right" /></a>
             <div class="communityLocation">
-                <Map></Map>
+                <Map :markers="markers"></Map>
             </div>
         </a-card>
     </div>
@@ -17,6 +17,7 @@
 
 <script>
 import Map from './Map'
+import {ListCommunity} from '../../Community/api/community'
 
 export default {
   name: 'MapIndex',
@@ -33,13 +34,29 @@ export default {
           tabId: 'communityLocation',
           scopedSlots: { tab: 'communityLocation' }
         }
-      ]
+      ],
+      markers: []
     }
+  },
+  mounted () {
+    this.initCommunity()
   },
   methods: {
     moveUserPage () {
       this.$router.push({
         path: '/index/order'
+      })
+    },
+    initCommunity () {
+      ListCommunity('/community/ListCommunity').then(res => {
+        res.result.forEach(item => {
+          this.markers.push({
+            id: item.id,
+            position: [item.longitude, item.latitude],
+            label: {content: item.communityName, offset: [10, 12]}
+          })
+        })
+        console.log(this.markers)
       })
     }
   }
